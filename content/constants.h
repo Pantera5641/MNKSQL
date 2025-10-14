98011d966797573e6cfbfcef619fa8078a660121
Pantera5641 <mazurov815@gmail.com>
2025-10-15 00:07:35 +0300

    Add constants library

---
+++ content/constants.h
@@ -0,0 +1,15 @@
+#pragma once
+#include <string>
+
+const char SPACE {' '};
+const char COMMA {','};
+const char SEMICOLON {';'};
+const char HYPHEN {'-'};
+const char LEFT_PARENTHESIS {'('};
+const char RIGHT_PARENTHESIS {')'};
+const char LEFT_SQUARE_BRACKET {'['};
+const char RIGHT_SQUARE_BRACKET {']'};
+const char VERTICAL_BAR {'|'};
+
+const std::string STRING {"string"};
+const std::string NONE {"none"};
