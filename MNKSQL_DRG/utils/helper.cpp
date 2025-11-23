486778a4cd431068de7f278492a9c255ab044d7d
Pantera5641 <mazurov815@gmail.com>
2025-11-23 15:42:51 +0300

    Add new func in helper

--- MNKSQL_DRG/utils/helper.cpp
+++ MNKSQL_DRG/utils/helper.cpp
@@ -12,3 +12,19 @@ std::string repeat(int num, char item)
     
     return result;
 }
+
+std::string cutAfter(const std::string& line, char item)
+{
+    int index = line.find_first_of(item);
+    std::string result = line.substr(0, index);
+
+    return result;
+}
+
+std::string cutBefore(const std::string& line, char item)
+{
+    int index = line.find_first_of(item);
+    std::string result = line.substr(index + 1);
+
+    return result;
+}
--- MNKSQL_DRG/utils/helper.h
+++ MNKSQL_DRG/utils/helper.h
@@ -3,3 +3,7 @@
 
 
 std::string repeat(int num, char item);
+
+std::string cutAfter(const std::string& line, char item);
+
+std::string cutBefore(const std::string& line, char item);
