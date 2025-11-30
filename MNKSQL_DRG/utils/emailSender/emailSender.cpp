09af5c05d28113a746d0d7e47dd8b8ce78005e63
Pantera5641 <mazurov815@gmail.com>
2025-11-30 18:49:18 +0300

    Add emailSender

---
+++ MNKSQL_DRG/utils/emailSender/emailSender.cpp
@@ -0,0 +1,17 @@
+#include "emailSender.h"
+
+
+void sendEmail(std::string userName, std::string emailAddress, std::string code)
+{
+    std::string params 
+    {
+        connect({userName, emailAddress, code, globalLanguage}, ' ')
+    };
+
+    #ifdef _WIN32
+        system(("EmailSender.exe " + params).c_str());
+    #else
+        system("chmod +x EmailSender");
+        system(("./EmailSender " + params).c_str());
+    #endif
+}
---
+++ MNKSQL_DRG/utils/emailSender/emailSender.h
@@ -0,0 +1,9 @@
+#include <iostream>
+#include <stdlib.h>
+#include <string>
+
+#include "utils/helper.h"
+#include "globals/globalLanguage.h"
+
+
+void sendEmail(std::string userName, std::string emailAddress, std::string code);
