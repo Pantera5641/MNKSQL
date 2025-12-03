6604cac031255a79fa3478b22ff2eea460b33cca
Pantera5641 <mazurov815@gmail.com>
2025-12-03 13:26:38 +0300

    Add async email send

--- MNKSQL_DRG/utils/emailSender/emailSender.cpp
+++ MNKSQL_DRG/utils/emailSender/emailSender.cpp
@@ -15,3 +15,9 @@ void sendEmail(std::string userName, std::string emailAddress, std::string code)
         system(("./EmailSender " + params).c_str());
     #endif
 }
+
+void sendEmailAsync(std::string userName, std::string emailAddress, std::string code)
+{
+    std::thread t(sendEmail, userName, emailAddress, code);
+    t.detach();
+}
--- MNKSQL_DRG/utils/emailSender/emailSender.h
+++ MNKSQL_DRG/utils/emailSender/emailSender.h
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string>
+#include <thread>
 
-#include "utils/helper.h"
+#include "helper/helper.h"
 #include "globals/globalLanguage.h"
 
 
 void sendEmail(std::string userName, std::string emailAddress, std::string code);
+
+void sendEmailAsync(std::string userName, std::string emailAddress, std::string code);
