3811b0f6c6e69d8438b5a86db8d4713324fbef6c
Pantera5641 <mazurov815@gmail.com>
2025-12-07 23:35:24 +0300

    Add a clear entry point to the version

--- main.cpp
+++ main.cpp
@@ -7,11 +7,21 @@
 int main()
 {
     std::string input {};
-    std::cin >> input;
-    std::cout << "\033c";
 
-    if (std::stoi(input) == 0)
-        startMain();
+    while (true) 
+    {
+        std::cout << "To start MNKSQL-MAIN enter \"Start Main\"" << std::endl;
+        std::cout << "Attention does not meet the limits" << std::endl;
+        std::cout << "or" << std::endl;
+        std::cout << "To start MNKSQL-DRG enter \"2\"" << std::endl;
 
-    startDrg();
+        std::cin >> input;
+        std::cout << "\033c";
+
+        if (input == "Start Main")
+            startMain();
+
+        if (input == "2")
+            startDrg();
+    }
 }
