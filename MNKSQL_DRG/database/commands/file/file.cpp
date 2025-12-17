893e5273fe4afaa5fd29eb6332a2c035e635168d
Pantera5641 <mazurov815@gmail.com>
2025-12-17 16:36:49 +0300

    fix bin files

--- MNKSQL_DRG/database/commands/file/file.cpp
+++ MNKSQL_DRG/database/commands/file/file.cpp
@@ -1,10 +1,19 @@
 #include "file.h"
-#include "dataStore/dataStore.h"
 
+struct BinStudent
+{
+    char lastName[1000] {};
+    char firstName[1000] {};
+    char surname[1000] {};
+    int yearOfBirth {};
+    int yearOfAdmission {};
+    int course {};
+    char group[1000] {};
+};
 
 void saveTxt(const std::string& fileName)
 {
-    std::fstream file(TO_TABLE_PATH + fileName + ".txt", std::ios::out | std::ios::app);
+    std::fstream file(TO_TABLE_PATH + fileName + ".txt", std::ios::out);
     for (int i = 0; i < studentsList.size(); i++) 
     {
         file << connect(getLine(i), ',') << '\n';
@@ -13,14 +22,22 @@ void saveTxt(const std::string& fileName)
 
 void saveBin(const std::string& fileName)
 {
-    std::fstream file(TO_TABLE_PATH + fileName + ".bin", std::ios::out | std::ios::app | std::ios::binary);
+    std::fstream file(TO_TABLE_PATH + fileName + ".bin", std::ios::out | std::ios::binary);
 
     for (int i = 0; i < studentsList.size(); i++)
     {
-        std::string item {connect(getLine(i), ',')};
-        int len = item.size();
-        file.write(reinterpret_cast<const char*>(&len), sizeof(len));
-        file.write(item.data(), len);  
+        Student student {studentsList.at(i)};
+        
+        BinStudent binStudent {};
+        strcpy_s(binStudent.lastName, sizeof(binStudent.lastName), student.lastName.c_str());
+        strcpy_s(binStudent.firstName, sizeof(binStudent.firstName), student.firstName.c_str());
+        strcpy_s(binStudent.surname, sizeof(binStudent.surname), student.surname.c_str());
+        binStudent.yearOfBirth = student.yearOfBirth;
+        binStudent.yearOfAdmission = student.yearOfAdmission;
+        binStudent.course = student.course;
+        strcpy_s(binStudent.group, sizeof(binStudent.group), student.group.c_str());
+        
+        file.write((char *)&binStudent, sizeof(binStudent));
     }
 }
 
@@ -41,19 +58,24 @@ std::vector<std::string> loadTxt(const std::string& fileName)
 
 std::vector<std::string> loadBin(const std::string& fileName)
 {
-    std::fstream file(TO_TABLE_PATH + fileName, std::ios::in | std::ios::binary);
-    std::vector<std::string> lines {};
+    std::fstream file(TO_TABLE_PATH + fileName + ".bin", std::ios::in  | std::ios::binary);
 
-    while (file.peek() != EOF) 
+    std::vector<std::string> rawData {};
+    BinStudent binStudent {};
+
+    while (file.read((char *)&binStudent, sizeof(binStudent))) 
     {
-        int len {};
-        file.read(reinterpret_cast<char*>(&len), sizeof(len));
+        Student student {};
+        student.lastName = binStudent.lastName;
+        student.firstName = binStudent.firstName;
+        student.surname = binStudent.surname;
+        student.yearOfBirth = binStudent.yearOfBirth;
+        student.yearOfAdmission = binStudent.yearOfAdmission;
+        student.course = binStudent.course;
+        student.group = binStudent.group;
 
-        std::string line(len, '\0');
-        file.read(line.data(), len);
-
-        lines.push_back(line);
+        rawData.push_back(connect(getLine(student), ','));
     }
 
-    return lines;
+    return rawData;
 }
